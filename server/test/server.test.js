898ef2f41f55abbd60efd167559f0a4b396a7a1f
Pranay Bhawsar <bhawsar@adobe.com>
2016-12-28 17:52:14 -0800

    added test cases for get route by id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,16 +1,18 @@
 // test file
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-const todos = [{"text":"First record"},{"text":"Second record"}];
+const todos = [{ _id: new ObjectID(), text: "First record" }
+    , { _id: new ObjectID(), text: "Second record" }];
 
 beforeEach((done) => {
     Todo.remove({}).then(() => {
-       return Todo.insertMany(todos);
-    }).then(()=>done());
+        return Todo.insertMany(todos);
+    }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -28,7 +30,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find({text}).then((todos) => {
+                Todo.find({ text }).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -37,7 +39,7 @@ describe('POST /todos', () => {
             })
     });
 
-    it('should not creatre todo with invalid data',(done)=>{
+    it('should not creatre todo with invalid data', (done) => {
 
         request(app)
             .post('/todos')
@@ -60,16 +62,40 @@ describe('POST /todos', () => {
     });
 });
 
-describe('GET /todos',()=>{
+describe('GET /todos', () => {
 
-it('should get all the todos',(done)=>{
- request(app)
- .get('/todos')
- .expect(200)
- .expect((res)=>{
-     expect(res.body.todos.length).toBe(2);
- })
- .end(done);
+    it('should get all the todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
+            })
+            .end(done);
+    });
+
 });
 
+describe('GET /todos/:id', () => {
+    it('should get all the todos by id', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+    it('should return 404 if todo not found', (done) => {
+        request(app)
+            .get(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+    it('should return 404 for non object ids', (done) => {
+        request(app)
+            .get('/todos/123')
+            .expect(404)
+            .end(done);
+    });
 });
